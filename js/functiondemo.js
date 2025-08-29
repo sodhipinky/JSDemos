60c7ab442134f55647be4dd3d192016a10243927
Pinky Sodhi <pinky.sodhi@gmail.com>
2025-08-29 12:15:04 -0400

    create function expression and call it

--- js/functiondemo.js
+++ js/functiondemo.js
@@ -1,5 +1,17 @@
-function greet(name){
+// Function
+function greet(name) {
     return `Hello, ${name}!`; // Template literal
 }
 
 console.log(greet("Alice")); // Example usage
+
+console.log(add(5, 3)); // Example usage
+// Function Expression
+const add = function (firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+// hoisting: means you can call the function before its declaration
+// Functions are hoisted, meaning you can call them before their declaration in the code.
+// Function expressions are not hoisted, so you must define them before calling.
+// Function expressions are Immediately Invoked (IIFE), meaning they can be executed right after their definition.
