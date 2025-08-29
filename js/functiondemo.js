3893c6f97c85421fc7414f41967177a0d8f25106
Pinky Sodhi <pinky.sodhi@gmail.com>
2025-08-29 12:39:05 -0400

    add arrow function examples and improve comments in functiondemo.js

--- js/functiondemo.js
+++ js/functiondemo.js
@@ -12,8 +12,18 @@ const add = function (firstNumber, secondNumber) {
 }
 console.log(add(5, 3)); // Example usage
 
-
 // hoisting: means you can call the function before its declaration
 // Functions are hoisted, meaning you can call them before their declaration in the code.
 // Function expressions are not hoisted, so you must define them before calling.
 // Function expressions are Immediately Invoked (IIFE), meaning they can be executed right after their definition.
+
+// Arrow Function, is a shorter syntax for writing functions
+
+const multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
+
+const calculate = (firstNumber, secondNumber) => {
+    console.log(firstNumber + secondNumber);
+    console.log(firstNumber - secondNumber);   
+    console.log(firstNumber * secondNumber);
+    console.log(firstNumber / secondNumber);
+}
