9ab3f01ae48bb27c4a8ff6c57992f6cd5776a991
Pinky Sodhi <pinky.sodhi@gmail.com>
2025-09-08 11:24:55 -0400

    demonstrate ways of  array creation

---
+++ js/arraydemo.js
@@ -0,0 +1,13 @@
+// An array is a special variable, which can hold more than one value
+// []
+let cars = ["Saab", "Volvo", "BMW"];
+
+let number = [1, 2, 3, 4, 5];
+
+let mixed = ["text", 1, true, null];
+
+let fruits = new Array("Apple", "Banana", 1, 5, 8);
+
+console.log(fruits);
+console.log(typeof(fruits));
+//iterate an array
