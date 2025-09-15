e679cc53c69d8200865357b1df7b1540b185dfa9
Pinky Sodhi <pinky.sodhi@gmail.com>
2025-09-15 11:53:19 -0400

    create an object with methods

--- js/objectdemo.js
+++ js/objectdemo.js
@@ -3,9 +3,9 @@ let student = {
     age: 21,
     isEnrolled: true
 }
-console.log(student);
-console.log(student.name);
-console.log(student["age"]);
+// console.log(student);
+// console.log(student.name);
+// console.log(student["age"]);
 
 //iterate an object using for...in loop
 /*
@@ -13,6 +13,26 @@ console.log(student["age"]);
         code block to be executed
     }
 */
-for (let index in student) {
-    console.log(`${index}: ${student[index]}`);
+// for (let index in student) {
+//     console.log(`${index}: ${student[index]}`);
+// }
+
+//Object methods
+let car = {
+    brand: "Toyota",
+    model: "Camry",
+    year: 2020,
+    speed: 0,
+
+    //method
+    accelerate(){
+        this.speed += 10;
+        console.log(`The car is accelerating. Current speed: ${this.speed} km/h`);
+    },
+
+    brake(){
+        this.speed -= 10;
+        if(this.speed < 0) this.speed = 0;
+        console.log(`The car is braking. Current speed: ${this.speed} km/h`);
+    }
 }
