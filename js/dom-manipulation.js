5396e298b41f0f8d5216557c87a7bbb414a5b9e9
Pinky Sodhi <pinky.sodhi@gmail.com>
2025-09-22 16:49:01 -0400

    add button functionality to change heading text on click

--- js/dom-manipulation.js
+++ js/dom-manipulation.js
@@ -2,4 +2,10 @@
 // document.querySelector("css selector")
 const heading = document.querySelector("h1");
 console.log(heading.innerText);
-heading.style.color = "blue";
+
+const button = document.getElementById("btnClick");
+
+function changeHeadingText() {
+    heading.innerText = "You clicked the button!";
+    button.style.backgroundColor = "green";
+}
