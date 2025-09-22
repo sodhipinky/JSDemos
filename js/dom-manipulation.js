1bb633cb23dd1d7e7369c519967545b317564208
Pinky Sodhi <pinky.sodhi@gmail.com>
2025-09-22 17:06:16 -0400

    add onclick functionality to the "Add Item" button

--- js/dom-manipulation.js
+++ js/dom-manipulation.js
@@ -9,3 +9,16 @@ function changeHeadingText() {
     heading.innerText = "You clicked the button!";
     button.style.backgroundColor = "green";
 }
+
+//document.createElement("li")
+// const ul = document.createElement("ul");
+// const li = document.createElement("li");
+const addButton = document.getElementById("btnAdd");
+const ul = document.getElementById("list");
+
+function addListItem() {
+    const li = document.createElement("li");
+    li.innerText = "New Item";
+    // document.body.appendChild(ul);
+    ul.appendChild(li);
+}
